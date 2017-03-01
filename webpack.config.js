f3c61cd92c5d932ad40a5641c49e8056eb18b27c
Zac Collier <zacacollier@gmail.com>
2017-03-01 13:52:54 -0600

    Add configs and directories

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const { resolve } = require('path');
+const webpack     = require('webpack');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+
+    // bundle the client for webpack-dev-server
+    // connect it to the provided endpoint
+    'webpack-dev-server/client?http://localhost:8080',
+
+    // bundle the client for hot reloading
+    // 'only-' means only hot reload for successful updates
+    'webpack/hot/only-dev-server',
+
+    // app entry point
+    './index.js'
+  ],
+
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/'
+    // HMR needs to know where to load hot update chunks
+  },
+
+  context: resolve(__dirname, 'src'),
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    hot: true,
+    https: true,
+    contentBase: resolve(__dirname, 'dist'),
+    publicPath: '/',
+    quiet: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?modules',
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    // global HMR
+    new webpack.HotModuleReplacementPlugin(),
+
+    // print more readable module names in the browser console
+    new webpack.NamedModulesPlugin(),
+  ]
+};
