f97830f22707b00bc7b2f48fe6561743396a8a81
Zac Collier <zacacollier@gmail.com>
2017-03-31 19:34:41 -0500

    Add plain HTTP config, update start scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,18 @@
+process.traceDepracation = true;
 const { resolve } = require('path');
 const webpack     = require('webpack');
 
 module.exports = {
   entry: [
     'react-hot-loader/patch',
-
     // bundle the client for webpack-dev-server
     // connect it to the provided endpoint
     'webpack-dev-server/client?http://localhost:8080',
-
     // bundle the client for hot reloading
     // 'only-' means only hot reload for successful updates
     'webpack/hot/only-dev-server',
-
     // app entry point
+    // rooted at './src'
     './index.js'
   ],
 
@@ -23,27 +22,25 @@ module.exports = {
     publicPath: '/'
     // HMR needs to know where to load hot update chunks
   },
-
   context: resolve(__dirname, 'src'),
 
   devtool: 'inline-source-map',
 
   devServer: {
     hot: true,
-    https: true,
     contentBase: resolve(__dirname, 'dist'),
     publicPath: '/',
-    quiet: true
   },
 
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: [
-          'babel-loader',
-        ],
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        },
       },
       {
         test: /\.css$/,
@@ -55,12 +52,10 @@ module.exports = {
       },
     ],
   },
-
   plugins: [
     // global HMR
     new webpack.HotModuleReplacementPlugin(),
-
     // print more readable module names in the browser console
     new webpack.NamedModulesPlugin(),
-  ]
+  ],
 };
